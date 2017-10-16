7f9924f27b112f8517823b813ecd7bcc9d7c74c8
Atracks <Atracks@bk.ru>
2017-10-17 01:02:05 +0500

    Init app
    Init Spring Boot App

---
+++ src/main/java/ru/bravery_and_stupidity/secretOfSatan/Application.java
@@ -0,0 +1,17 @@
+package ru.bravery_and_stupidity.secretOfSatan;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  private static final Logger logger = Logger.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    BasicConfigurator.configure();
+    SpringApplication.run(Application.class, args);
+    logger.info("Secret of satan was running");
+  }
+}
