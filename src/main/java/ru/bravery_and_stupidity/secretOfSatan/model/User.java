59a1966df342815a064ce2f69ca0479d6bb4f23b
Atracks <Atracks@bk.ru>
2017-10-17 23:02:05 +0500

    Added model for user entity

---
+++ src/main/java/ru/bravery_and_stupidity/secretOfSatan/model/User.java
@@ -0,0 +1,36 @@
+package ru.bravery_and_stupidity.secretOfSatan.model;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public interface User {
+
+    void setName(@NotNull String name);
+
+    void setLogin(@NotNull String login);
+
+    void setPassword(@NotNull String password);
+
+    void setDesire(@NotNull String desire);
+
+    void setAdmin(boolean admin);
+
+    void setTarget(int target);
+
+    @NotNull
+    String getName();
+
+    @NotNull
+    String getLogin();
+
+    @NotNull
+    String getPassword();
+
+    @NotNull
+    String getDesire();
+
+    boolean isAdmin();
+
+    int getTarget();
+
+}
